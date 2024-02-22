6c902ab36791165b228b625f3a94e1b7a60a8993
AridArab <zohairalsheikh2002@gmail.com>
2024-02-22 11:38:55 -0600

    Created button to use for links on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import NavBar from './NavBar.jsx';
 import HomeAnimationOne from './components/Home/HomeAnimationOne.jsx';
 import HomeAnimationTwo from './components/Home/HomeAnimationTwo.jsx';
+//import ButtonToPage from './components/Home/ButtonToPage.jsx'
 
 
 function Home() {
