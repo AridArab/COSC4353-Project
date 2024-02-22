288d9e1ea00864894002c98f2713c37c54e968c4
AridArab <zohairalsheikh2002@gmail.com>
2024-02-22 14:01:41 -0600

    Fixed classes in some files

--- src/components/Profile/StateSelectorDropdown.jsx
+++ src/components/Profile/StateSelectorDropdown.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function StateSelectorDropdown(props){
   return(
-    <select id="country-state" name="country-state" class={props.class}>
+    <select id="country-state" name="country-state" className={props.className}>
       <option value="">Select state</option>
       <option value="AL">Alabama</option>
       <option value="AK">Alaska</option>
@@ -60,7 +60,7 @@ function StateSelectorDropdown(props){
 }
 
 StateSelectorDropdown.propTypes = {
-  class: PropTypes.string
+  className: PropTypes.string
 }
 
 export default StateSelectorDropdown;
--- src/components/QuoteForm/QuoteFormContainer.jsx
+++ src/components/QuoteForm/QuoteFormContainer.jsx
@@ -4,29 +4,29 @@ function QuoteFormContainer(){
 
 
     return(
-        <div class="quote-form-container">
+        <div className="quote-form-container">
         <form id="fuelQuoteForm">
         <h1> Fuel Quote Form</h1>
         <h2>Please fill form below</h2>
-        <div class="quote-form-bubble">
-            <label for="gallonsRequested">Gallons Requested</label>
+        <div className="quote-form-bubble">
+            <label htmlFor="gallonsRequested">Gallons Requested</label>
             <input type="number" id="gallonsRequested" name="gallonsRequested" required></input>
         </div>
-        <div class="quote-form-bubble">
-            <label for="deliveryAddress">Delivery Address</label>
-            <input type="text" id="deliveryAddress" name="deliveryAddress" readonly></input>
+        <div className="quote-form-bubble">
+            <label htmlFor="deliveryAddress">Delivery Address</label>
+            <input type="text" id="deliveryAddress" name="deliveryAddress" readOnly></input>
         </div>
-        <div class="quote-form-bubble">
-            <label for="deliveryDate">Delivery Date</label>
+        <div className="quote-form-bubble">
+            <label htmlFor="deliveryDate">Delivery Date</label>
             <input type="date" id="deliveryDate" name="deliveryDate" required></input>
         </div>
-        <div class="quote-form-bubble">
-            <label for="suggestedPrice">Suggested Price / Gallon:</label>
-            <input type="number" id="suggestedPrice" name="suggestedPrice" readonly></input>
+        <div className="quote-form-bubble">
+            <label htmlFor="suggestedPrice">Suggested Price / Gallon:</label>
+            <input type="number" id="suggestedPrice" name="suggestedPrice" readOnly></input>
         </div>
-        <div class="quote-form-bubble">
-            <label for="totalAmountDue">Total Amount Due</label>
-            <input type="number" id="totalAmountDue" name="totalAmountDue" readonly></input>
+        <div className="quote-form-bubble">
+            <label htmlFor="totalAmountDue">Total Amount Due</label>
+            <input type="number" id="totalAmountDue" name="totalAmountDue" readOnly></input>
         </div>
         <button type="button" id="editProfile">Edit Profile</button>
         <button type="button" id="confirmOrder">Confirm Order</button>
